2e8932b2b0ba4bdf875a1f24ef943f919fde7ad8
Nick Felicelli <nick.felicelli@gmail.com>
2018-08-02 11:45:01 -0400

    Completing module4

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,15 +11,14 @@ class Quiz extends Component {
   };
 
   render() {
-    const { quiz_position } = this.state;
-    const isQuizEnd = quizData.quiz_questions.length === quiz_position - 1
+    const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
 
     return (
       <div>
         {isQuizEnd ? (
           <QuizEnd />
         ) : (
-          <QuizQuestion quiz_question={quizData.quiz_questions[quiz_position - 1]}/>
+          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
         )}
       </div>
     )
